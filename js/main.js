558ed8771e6539333c8d0e0ed630f4d17a7a2fb4
shweshi <contactmespg@gmail.com>
2021-02-16 00:08:37 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,139 @@
+var default_text = "Enter or paste your text here. To download and save it, click on the Download button.";
+var cleanRequired = 1;
+var fullScreen = 0;
+
+function getRandomQuote() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200 && cleanRequired) {
+            response = JSON.parse(this.responseText);
+            document.getElementById("text").innerHTML = response.content + ' ~ ' + response.author;
+        }
+    };
+    xhttp.open("GET", "https://api.quotable.io/random", true);
+    xhttp.send();
+}
+
+function clearText() {
+    document.textform.text.value = '';
+}
+
+var elem = document.documentElement;
+
+function openFullscreen() {
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+    }
+    document.getElementById("fullScreen").style.display = "none";
+    document.getElementById("closeFullScreen").style.display = "initial";
+}
+
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+        document.msExitFullscreen();
+    }
+    document.getElementById("fullScreen").style.display = "initial";
+    document.getElementById("closeFullScreen").style.display = "none";
+}
+
+function download() {
+    var text = document.getElementById("text").value;
+    text = text.replace(/\n/g, "\r\n"); // To retain the Line breaks.
+    var blob = new Blob([text], { type: "text/plain" });
+    var anchor = document.createElement("a");
+    anchor.download = "text.txt";
+    anchor.href = window.URL.createObjectURL(blob);
+    anchor.target = "_blank";
+    anchor.style.display = "none"; // just to be safe!
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+}
+
+function clock() {
+    var clockElement = document.getElementById('time');
+    clockElement.textContent = new Date().toISOString();
+}
+
+setInterval(clock, 1000);
+
+function toggleDayNight() {
+    applyDarkMode();
+
+    (localStorage.getItem("dark-mode") === 'true') ? localStorage.setItem("dark-mode", false) : localStorage.setItem("dark-mode", true);
+}
+
+function applyDarkMode() {
+    var navbar = document.getElementById("navbar");
+    navbar.classList.toggle("dark-mode-header");
+
+    var buttons = document.getElementsByClassName("header-button");
+    for (i = 0; i < buttons.length; i++) {
+        buttons[i].classList.toggle("dark-mode-header-button");
+    }
+
+    var textarea = document.getElementById("text");
+    textarea.classList.toggle("dark-mode-textarea");
+
+    var anchors = document.getElementsByTagName('a');
+    for (i = 0; i < anchors.length; i++) {
+        anchors[i].classList.toggle("dark-mode-a");
+    }
+
+    var dropdown = document.getElementsByClassName("dropdown-content");
+    for (i = 0; i < dropdown.length; i++) {
+        dropdown[i].classList.toggle("dark-mode-dropdown-content");
+    }
+}
+
+function checkDarkMode() {
+    var darkMode = localStorage.getItem("dark-mode");
+    if (darkMode === 'true') {
+        applyDarkMode();
+    }
+}
+
+function updateBackgroundColor(color) {
+    document.getElementById("text").style.backgroundColor = color;
+}
+
+function updateFontColor(color) {
+    document.getElementById("text").style.color = color;
+}
+
+function showMore() {
+    document.getElementById("more-dropdown").classList.toggle("show");
+}
+
+function resizeText(multiplier) {
+    if (document.getElementById("text").style.fontSize == "") {
+        document.getElementById("text").style.fontSize = "1.0em";
+    }
+    document.getElementById("text").style.fontSize = parseFloat(document.getElementById("text").style.fontSize) + (multiplier * 0.2) + "em";
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+    if (event.target.matches('.font') || event.target.matches('.color-button')) {
+        event.stopPropagation();
+    } else {
+        if (!event.target.matches('.dropbtn')) {
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
+}
